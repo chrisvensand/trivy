8fd61d6be858827661a28477e137a44c5548b8f1
Christopher Vensand <chrisvensand@gmail.com>
2023-12-20 12:06:31 -0800

    add basic navigation bar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from '../../components/NavigationBar'
+
+export default function About() {
+  return (
+    <div>
+      <NavigationBar />
+      <h1>About Trivy Trivia</h1>
+      <p>This is a trivia web application built with Next.js and TypeScript.</p>
+    </div>
+  )
+}
